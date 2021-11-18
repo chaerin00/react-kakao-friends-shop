10590f27fb974b967e7fcacf3a601382479f11fd
CHAERIN AN <72637095+chaerin00@users.noreply.github.com>
2021-11-18 11:28:01 +0900

    chore: add test code (#1)
    * chore: add test code
    
    * fix: fix ci error

--- src/pages/My.js
+++ src/pages/My.js
@@ -1,19 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import KakaoLogin from "react-kakao-login";
 import { responseKakao } from "../lib/api";
 
 const My = () => {
-  const [isLogin, setIsLogin] = useState(false);
-
-  useEffect(() => {
-    localStorage.getItem("token") ? setIsLogin(true) : setIsLogin(false);
-  }, []);
-  return (
-    <KakaoLogin
-      jsKey={"2ddc176ab543acd5e95ff7f0bbfc2683"}
-      onSuccess={responseKakao}
-    />
-  );
+  return <KakaoLogin jsKey={"2ddc176ab543acd5e95ff7f0bbfc2683"} onSuccess={responseKakao} />;
 };
 
 export default My;
